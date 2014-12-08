f10c9dab05fe8e58045fc136081565c20ac40016
victor tejada yau <victortyau@gmail.com>
2014-12-08 01:04:47 -0500

    working on c++

--- Data Recovery/data_recovery.cpp
+++ Data Recovery/data_recovery.cpp
@@ -1,37 +1,90 @@
 #include <stdio.h>
+#include <string.h>
+#include <cctype>
 #include<fstream>
 #include<iostream>
 using namespace std;
 
 class Main{
   public:
-    void gotFile(char filename[100]){
-      char lines[80];
-      int i = 0;
-      char array_lines[100][80];
-      printf("running in gotFile \n");
-      printf("%s\n",filename);
-      ifstream fin(filename);
-      if(fin){
-        printf("the file exist \n");
-        while(!fin.eof()){
-            fin >> array_lines[i];
-          //  printf("%s \n",array_lines[i]);
-          printf("%d",i);
-            i++;
-        }
-      }
-      fin.close();
-    }
-    void makeProcess(){}
+    void gotFile(char filename[100]);
+    void makeProcess(char array_lines[100][200],int index,int array_length[100]);
+    void createText(char array_strings[100][80],int array_indexes[100], int x, int z);
 };
 
-int main(int argc, char* argv[]){
+void Main::gotFile(char filename[100]){
+  int i = 0;
+  string line;
+  char array_lines[100][200];
+  int array_length[100];
+  ifstream fin(filename);
+  if(fin.is_open()){
+    while(fin.good()){
+      getline(fin,line);
+      if(!line.empty()){
+        array_length[i] = line.length();
+        strcpy(array_lines[i], line.c_str());
+        i++;
+      }
+    }
+    fin.close();
+    makeProcess(array_lines,i,array_length);
+  }
+}
+void Main::makeProcess(char array_lines[100][200],int index,int array_length[100]){
+  int x = 0;
+  int y = 0;
+  int z = 1;
+  int n = 1;
+  char tol[10] = ";";
+  char array_strings[100][80];
+  int array_indexes[100];
+  for(int i=0; i < index; i++){
+    for( int j =0; j < array_length[i]; j++ ){
+      if( array_lines[i][j] == tol[0] || n >= 2 ){
+        if(!isspace(array_lines[i][j]) && array_lines[i][j] != tol[0]){
+          array_indexes[z] = (int)array_lines[i][j];
+          z++;
+        }
+        n++;
+      }
+      else if(!isspace(array_lines[i][j])){
+        array_strings[x][y] = array_lines[i][j];
+        y++;
+      }
+      else{
+        x++;
+        y=0;
+      }
+    }
+    createText(array_strings,array_indexes,x,z);
+    break;
+  }
+}
 
+void Main::createText(char array_strings[100][80],int array_indexes[100], int x, int z){
+
+
+ for( int i =0; i < x; i++){
+    printf("%s \n",array_strings[i]);
+ }
+
+
+  if(x == z){
+    printf("the same indexes");
+    printf("%d\n",x);
+    printf("%d\n",z);
+  }
+  else{
+    printf("the wrong indexes \n");
+    printf("%d\n",x);
+    printf("%d\n",z);
+  }
+}
+
+int main(int argc, char* argv[]){
     if( argc == 2 ){
-      printf("running c++ \n");
       Main n;
-      printf("%s",argv[1]);
       n.gotFile(argv[1]);
     }
 }
