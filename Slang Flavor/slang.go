b87f1724bee30a150a97fa31ea8c7977189812e6
victortyau <victortyau@gmail.com>
2015-01-24 10:59:20 -0500

    working in slang flavor challenge

--- Slang Flavor/slang.go
+++ Slang Flavor/slang.go
@@ -2,9 +2,37 @@ package main
 
 import ( "fmt"
          "io/ioutil"
-"strings"
-"regexp"
-"os")
+         "strings"
+         "os"
+         )
 
 
-func main(){}
+func main(){
+  getFile()
+}
+
+func getFile(){
+  content, err := ioutil.ReadFile(os.Args[1])
+
+  if err != nil {
+    fmt.Println("Problems to read file")
+    return
+  }
+
+  lines := strings.Split(strings.Trim(string(content),"\n"),"\n")
+
+  doSlang(lines)
+}
+
+
+func doSlang(lines []string){
+
+
+    for i := 0 ; i < len(lines); i++ {
+      current_line := strings.Split(lines[i]," ")
+      for j := 0; j < len(current_line); j++ {
+        fmt.Println(current_line[j])
+      }
+      fmt.Println("========================")
+    }
+}
