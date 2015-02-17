61c8d8db1f60f6edff26e3566f77ec8be17ffa55
victor tejada yau <victortyau@gmail.com>
2015-02-16 21:32:35 -0500

    almost done again slang flavor

--- Slang Flavor/slang.go
+++ Slang Flavor/slang.go
@@ -36,22 +36,27 @@ func doSlang(lines []string){
     for i := 0 ; i < len(lines); i++ {
         if !markexp.MatchString(lines[i]) {
          current_line := strings.Split(lines[i]," ")
-         x = 0
+         var x = 0
          for j := 0; j < len(current_line); j++ {
            if strings.Index(current_line[j],".") >= 0 {
-             //index := strings.Index(current_line[j],".")
-             strings.Replace(current_line[j],".",slang_array[x])
-             
+             fmt.Printf(strings.Replace(current_line[j],".",slang_array[x],1)+" ")
+             x++
+           }else if  strings.Index(current_line[j],"?") >= 0 {
+             fmt.Printf(strings.Replace(current_line[j],"?",slang_array[x],1)+" ")
+             x++
+           }else if strings.Index(current_line[j],"!") >= 0 {
+             fmt.Printf(strings.Replace(current_line[j],"?",slang_array[x],1)+" ")
+             x++
+           }else{
+             fmt.Printf(current_line[j]+" ")
            }
-           else if  strings.Index(current_line[j],"?") >= 0 {
-             index := strings.Index(current_line[j],"?")
-           }
-           else if strings.Index(current_line[j],"!") >= 0 {
-             index := strings.Index(current_line[j],"!")
+
+           if x == len(slang_array) {
+             x = 0
            }
          }
         }
+        fmt.Println("")
         break
-        fmt.Println("========================")
       }
 }
