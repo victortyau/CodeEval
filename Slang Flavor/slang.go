f0ff769f5a3048f8e65389199ebc43bcfc31677a
victor tejada yau <victortyau@gmail.com>
2015-02-18 20:10:24 -0500

    working on slang flavor

--- Slang Flavor/slang.go
+++ Slang Flavor/slang.go
@@ -31,38 +31,44 @@ func doSlang(lines []string){
     slang_array :=  [8]string{ ", yeah!" , ", this is crazy, I tell ya.", ", can U believe this?",
                                ", eh?", ", aw yea.", ", yo.", "? No way!", ". Awesome!" }
     markexp := regexp.MustCompile(`([\.]|[\?]|[\!]){2,}`)
-    counter := regexp.MustCompile(`[\!]|[\.]|[\?]|[\,]`)
+    counter := regexp.MustCompile(`[\!]`)
+    counter_2 := regexp.MustCompile(`[\.]`)
+    counter_3 := regexp.MustCompile(`[\?]`)
     var x = 0
     for i := 0 ; i < len(lines); i++ {
         number := 0
-        stop := 0
+        number_2 := 0
+        number_3 := 0
+        //stop := 0
 
         if !markexp.MatchString(lines[i]) {
          current_line := strings.Split(lines[i]," ")
 
          for j := 0; j < len(current_line); j++ {
 
+             if counter.MatchString(current_line[j]) {
+               number++
+             }else if counter_2.MatchString(current_line[j]) {
+               number_2++
+             }else if counter_3.MatchString(current_line[j]) {
+               number_3++
+             }
 
-           if counter.MatchString(current_line[j])  {
-             number++
-           }
 
-           if number >= 2 && stop == 0 {
-
-              if strings.Index(current_line[j],".") >= 0  {
+              if strings.Index(current_line[j],".") >= 0 && number%2 ==0 && number > 0  {
                 fmt.Printf(strings.Replace(current_line[j],".",slang_array[x],1)+" ")
                 x++
-              }else if strings.Index(current_line[j],"?") >= 0 {
+              }else
+              if strings.Index(current_line[j],"?") >= 0 && number_3 > 0 {
                 fmt.Printf(strings.Replace(current_line[j],"?",slang_array[x],1)+" ")
                 x++
-              }else if strings.Index(current_line[j],"!") >= 0 {
+              }else
+              if strings.Index(current_line[j],"!") >= 0 && number_2 % 2 == 0 && number_2 > 0 {
                 fmt.Printf(strings.Replace(current_line[j],"!",slang_array[x],1)+" ")
                 x++
+              }else{
+                fmt.Print(current_line[j]+" ")
               }
-              stop++
-           }else{
-             fmt.Print(current_line[j]+" ")
-           }
 
          }
          fmt.Println("")
