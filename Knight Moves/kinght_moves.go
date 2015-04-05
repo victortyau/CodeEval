f37576575acdd987f92c912d742b631ba08263da
victor tejada yau <victortyau@gmail.com>
2015-04-04 22:34:55 -0500

    working on knight moves

--- Knight Moves/kinght_moves.go
+++ Knight Moves/kinght_moves.go
@@ -12,13 +12,17 @@ func main(){
 
 func gotFiles(){
   content, err := ioutil.ReadFile(os.Args[1])
-
   if err != nil {
     fmt.Println("Problems to read file")
     return
   }
-
   lines := strings.Split(strings.Trim(string(content),"\n"),"\n")
+  if len(lines) {
 
-  fmt.Println(len(lines))
+  }
+}
+
+func knightMoves(moves []string){
+  letter := ["a","b","c","d","e","f","g","h"]
+  number := ["8","7","6","5","4","3","2","1"]
 }
